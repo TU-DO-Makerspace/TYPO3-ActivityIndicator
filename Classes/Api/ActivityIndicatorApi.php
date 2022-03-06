c34e5e990c97509657e5fa95d0494a106c8bce57
Patrick Pedersen <ctx.xda@gmail.com>
2022-03-06 01:56:21 +0100

    API: First working API
    Signed-off-by: Patrick Pedersen <ctx.xda@gmail.com>

--- Classes/Api/ActivityIndicatorApi.php
+++ Classes/Api/ActivityIndicatorApi.php
@@ -2,16 +2,22 @@
 
 namespace TUDOMakerspace\Activityindicator\Api;
 
+use TYPO3\CMS\Extbase\Persistence\Generic\PersistenceManager;
+
 use Nng\Nnrestapi\Annotations as Api;
 use Nng\Nnrestapi\Api\AbstractApi;
 
 use TUDOMakerspace\Activityindicator\Domain\Model\ActivityIndicator;
 use TUDOMakerspace\Activityindicator\Domain\Repository\ActivityIndicatorRepository;
 
+/**
+ * @Api\Endpoint()
+ */
 class ActivityIndicatorApi extends AbstractApi
 {
     protected $activityIndicatorRepository = null;
-    
+    protected $persistenceManager = null;
+
     /**
      * @param ActivityIndicatorRepository $activityIndicatorRepository
      */
@@ -21,16 +27,55 @@ class ActivityIndicatorApi extends AbstractApi
     }
 
     /**
-     * action postActivity
+    * @param PersistenceManager $persistenceManager
+    */
+   public function injectPersistenceManager(PersistenceManager $persistenceManager)
+   {
+       $this->persistenceManager = $persistenceManager;
+   }
+
+    // TODO: Use a single post request instead of two
+    // TODO: Authenticate the request
+
+    /**
+     *
+     * action postOpenAction
      * Updates the activity status in the repository via
      * a REST POST call
      * 
-     * @param bool $status
+     * @Api\Access("public")
      * 
+     * @return array
      */
-    public function postActivityAction( $status )
+    public function postOpenAction()
     {
         $activityIndicator = $this->activityIndicatorRepository->getActivityIndicator();
-        // $this->activityIndicatorRepository->update($activityIndicator);
+        $activityIndicator->setActivity(true);
+        $this->activityIndicatorRepository->update($activityIndicator);
+        $this->persistenceManager->persistAll();
+        return [
+            'activity' => "Open",
+        ];
+    }
+
+    /**
+     *
+     * action postCloseAction
+     * Updates the activity status in the repository via
+     * a REST POST call
+     * 
+     * @Api\Access("public")
+     * 
+     * @return array
+     */
+    public function postCloseAction()
+    {
+        $activityIndicator = $this->activityIndicatorRepository->getActivityIndicator();
+        $activityIndicator->setActivity(false);
+        $this->activityIndicatorRepository->update($activityIndicator);
+        $this->persistenceManager->persistAll();
+        return [
+            'activity' => "Closed",
+        ];
     }
 }
