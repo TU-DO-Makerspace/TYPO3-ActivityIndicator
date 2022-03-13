89d6e5f57bd3867ebf0f34ed55eebbccb4e4e14d
CTXz <ctx.xda@gmail.com>
2022-03-13 20:16:07 +0100

    Added FlexForms to customize Open and Closed text
    Signed-off-by: CTXz <ctx.xda@gmail.com>

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,8 +1,16 @@
 <?php
 defined('TYPO3_MODE') || die();
 
+$pluginSignature = 'activityindicator_activityindicator';
+
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'Activityindicator',
     'Activityindicator',
     'ActivityIndicator'
+);
+
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+	$pluginSignature,
+	'FILE:EXT:activityindicator/Configuration/FlexForms/ActivityIndicator_ActivityIndicator.xml'
 );
